27a57aa007e5f88819ff4d2f07fcc4498552ed0c
Richard Araneda <richard._.xd@hotmail.com>
2022-11-24 01:25:58 -0300

    testeo

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,24 +1,26 @@
 import { AppPage } from './app.po';
+import {browser} from 'protractor';
 
 describe('new App', () => {
   let page: AppPage;
 
   beforeEach(() => {
+    browser.waitForAngularEnabled(false)
     page = new AppPage();
   });
 
   it('prueba 1', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toContain('Registrarse');
+    expect(page.getTitleText()).toContain('Registrarse');
   });
   
   it('prueba 2', () => {
     page.navigateTo();
-    expect(page.getParagraphText2()).toContain('Login');
+    expect(page.getTestp()).toContain('Login');
   });
 
 
-  it('prueba 1', () => {
+  it('prueba 3', () => {
     page.navigateTo();
     expect(page.getParagraphText3()).toContain('inicio');
   });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,17 +1,17 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element , promise} from 'protractor';
 
 export class AppPage {
   navigateTo() {
     return browser.get('/');
-  }
+  };
 
-  getParagraphText() {
-    return element(by.deepCss('app-root ')).getText();
-  }
-  getParagraphText2() {
-    return element(by.deepCss('app-root h2')).getText();
-  }
-  getParagraphText3() {
-    return element(by.deepCss('app-root h3')).getText();
-  }
+  async getTitleText():Promise<string>{
+    return element(by.deepCss('app-root <ion-card-title>')).getText();
+  };
+  async getTestp():Promise<string>{
+    return element(by.Css('app-root <p>')).getText();
+  };
+  async getParagraphText3():Promise<string>{
+    return element(by.Css('app-root h3')).getText();
+  };
 }
